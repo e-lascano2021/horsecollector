fb83b14c6ac61b4b8a3b848b47281ecf75a83385
e.lascano <emilas5678@gmail.com>
2021-11-21 12:17:55 -0500

    complete part 5

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from .models import Horse
+from django.views.generic import ListView, DetailView
+from .models import Horse, Toy
 from .forms import FeedingForm
 
 def home(request):
@@ -43,3 +44,20 @@ class HorseDelete(DeleteView):
   model = Horse
   success_url = '/horses/'
 
+class ToyCreate(CreateView):
+  model = Toy
+  fields = '__all__'
+
+class ToyList(ListView):
+  model = Toy
+
+class ToyDetail(DetailView):
+  model = Toy
+
+class ToyUpdate(UpdateView):
+  model = Toy
+  fields = ['name', 'color']
+
+class ToyDelete(DeleteView):
+  model = Toy
+  success_url = '/toys/'
